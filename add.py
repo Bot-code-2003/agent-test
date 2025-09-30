f95345d06ea9718d5e7ff34939c41cc6bd722567
Bot-code-2003 <dharmadeepmadisetty@gmail.com>
2025-09-30 11:55:45 +0530

    Automated commit

--- add.py
+++ add.py
@@ -1,6 +1,16 @@
 import random
 
-num = random.randint(1, 5)
-guess = int(input("Guess (1-5): "))
+def main():
+    try:
+        num = random.randint(1, 5)
+        guess = int(input("Guess a number between 1 and 5: "))
+        
+        if guess < 1 or guess > 5:
+            print("Invalid input. Please enter a number between 1 and 5.")
+        else:
+            print("Correct!" if guess == num else f"Wrong, it was {num}")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
 
-print("Correct!" if guess == num else f"Wrong, it was {num}")
+if __name__ == "__main__":
+    main()
