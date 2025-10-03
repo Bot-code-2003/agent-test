dfbdbedf48113af20f17e086d76a0c3d91a36899
Bot-code-2003 <dharmadeepmadisetty@gmail.com>
2025-10-03 15:59:12 +0530

    Automated commit

--- add.py
+++ add.py
@@ -1,6 +1,14 @@
 import random
 
+# Generate a random number between 1 and 5
 num = random.randint(1, 5)
-guess = int(input("Guess (1-5): "))
 
-print("Correct!" if guess == num else f"Wrong, it was {num}")
+# Input validation for the user's guess
+try:
+    guess = int(input("Guess (1-5): "))
+    if guess < 1 or guess > 5:
+        print("Please enter a number between 1 and 5.")
+    else:
+        print("Correct!" if guess == num else f"Wrong, it was {num}")
+except ValueError:
+    print("Invalid input. Please enter a number.")
